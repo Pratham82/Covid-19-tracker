152fa0f8cb1246fc57e3d9f062cdbee8bba12d6a
Pratham82 <mali.prathamesh82@gmail.com>
2020-04-23 21:28:05 +0530

    Cards fucntionality and css added'

---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,15 @@
+import React,{useState,useEffect} from 'react';
+
+const Chart = () => {
+    const[dailyData,setDailyData ]= useState({});
+
+    useEffect(()=>{
+
+    }); 
+    
+    return(
+        <h1>Chart</h1>  
+    )
+}
+
+export default Chart;
